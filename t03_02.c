47f9a3dd9e7a70e781bed01ee25686f9d559e505
clarasiasimanjuntak <clarasiasimanjuntak@gmail.com>
2024-02-06 21:40:55 +0700

    CLARASIA

--- t03_02.c
+++ t03_02.c
@@ -1,11 +1,39 @@
-// NIM - NAMA
+// 12S23019_CLARASIA L. SIMANJUNTAK
 
 #include <stdio.h>
 
-int main(int _argc, char **_argv)
-{
+int main() {
+    int n, i, nilai;
+    int min = 101; // Inisialisasi nilai minimum dengan nilai yang tidak mungkin dicapai
+    int max = -101; // Inisialisasi nilai maksimum dengan nilai yang tidak mungkin dicapai
+    int total = 0; // Variabel untuk menyimpan total nilai
 
-  // your code here
+    // Menerima masukan jumlah baris dan nilai-nilai
+    scanf("%d", &n);
+   // Menerima nilai-nilai dan mencari nilai minimum, maksimum, dan total
+    for (i = 0; i < n; i++) {
+        scanf("%d", &nilai);
+
+        // Memperbarui nilai minimum dan maksimum jika ditemukan nilai yang lebih kecil atau lebih besar
+        if (nilai < min) {
+            min = nilai;
+        }
+        if (nilai > max) {
+            max = nilai;
+        }
+
+        // Menambahkan nilai ke total
+        total += nilai;
+    }
+
+    // Menghitung nilai rata-rata
+    float rata_rata = (float)total / n;
+
+    // Menampilkan nilai minimum, maksimum, dan rata-rata
+    printf("%d\n", min);
+    printf("%d\n", max);
+     printf("%.2f\n", rata_rata);
+
 
   return 0;
 }
